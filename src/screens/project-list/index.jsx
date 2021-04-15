011fb885f19057f224ba620ec033fbdec6e5cba9
Tiar <tiar@TiardeMacBook-Pro.local>
2021-04-15 23:22:32 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react"
+import qs from "qs"
+
+import { List } from "./list"
+import { SearchPanel } from "./search-panel"
+import { cleanObject } from "../../utils/index"
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const Projectlist = () => {
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [users, setUsers] = useState([]);
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+            if (res.ok) {
+                setList(await res.json());
+            }
+        });
+    }, [param])
+
+    useEffect(() => {
+        fetch(`${apiUrl}/users`).then(async res => {
+            if (res.ok) {
+                setUsers(await res.json());
+            }
+        });
+    }, [])
+    
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}/>
+        <List list={list} users={users}/>
+    </div>
+}
