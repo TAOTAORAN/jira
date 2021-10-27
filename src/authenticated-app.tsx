e1838e472ce31d8e4ad2765334febc6fd5548afb
Tiar <tianruo.tian@hand-china.com>
2021-10-27 23:21:27 +0800

    feat: 使用grid和flex优化布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,66 @@
+import styled from "@emotion/styled";
+
 import { useAuth } from "context/auth-context";
 import { ProjectList } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectList />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
+        <ProjectList />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
+
+// gird使用场景：1. 二维布局 2. 适合知道网格数量的情况
+// flex使用场景：1. 一维布局 2. 适合内容数量不固定 后期可能会增减的情况
+
+const Container = styled.div`
+  display: grid;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-rows: 4rem 1fr 6rem;
+  height: 100vh;
+`;
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: gray;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+`;
--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
+import { Card } from "antd";
+import styled from "@emotion/styled";
+
 import { RegisterScreen } from "./register";
 import { LoginScreen } from "./login";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
 
 export const UnauthenticatedApp = () => {
   const [isRegister, setRegister] = useState(false);
   return (
-    <div>
-      {isRegister ? <RegisterScreen /> : <LoginScreen />}
-      <button onClick={() => setRegister(!isRegister)}>
-        切换到{isRegister ? "登录" : "注册"}
-      </button>
-    </div>
+    <Container>
+      <Card>
+        {isRegister ? <RegisterScreen /> : <LoginScreen />}
+        <button onClick={() => setRegister(!isRegister)}>
+          切换到{isRegister ? "登录" : "注册"}
+        </button>
+      </Card>
+    </Container>
   );
 };
